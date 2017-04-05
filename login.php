4ae2c92c1e464657eb97842d38ebe36f23ca9ba8
AnaRome <tashianahatcher@gmail.com>
2017-04-05 03:10:08 -0400

    Update login.php
    HTML for login page

--- login.php
+++ login.php
@@ -1,1 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
 
+<h1>Login</h1>
+
+<form action = "loginPage.php">
+
+   <div class = "container">
+     <label><b>Username</b></label>
+     <input type = "text" placeholder="Enter Username" name ="username" required>
+   </div>
+    
+   <div class = "container">
+     <label><b>Password</b></label>
+     <input type = "text" placeholder="Enter Password" name ="password" required>
+
+     <button type ="submit">Login</button>
+     <input type ="checkbox" checked = "checked"> Remember me
+   </div>
+
+   <div clas ="container">
+     <button type = "button" class ="cancelbtn">Cancel</button>
+     <button type = "button" class ="forgotbtn">Forgot Password?</button>
+   </div>
+</form>
+
+</body>
+</html>
