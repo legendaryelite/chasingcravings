3a6ada5886e1dd53d5887007c72645fca4483f95
colsson0 <colsson0@gmail.com>
2017-04-07 19:57:54 -0400

    Create headInfo.php

---
+++ headInfo.php
@@ -0,0 +1,9 @@
+<?php
+	// This php file is designed to reduce clutter by putting all of the <head> information in the same place.
+	echo " <meta charset=\"utf-8\">
+	<title>Chasing Cravings</title>
+		<meta name=\"description\" content=\"A locating website to help you keep up with your favorite food trucks!\">
+		<meta name=\"author\" content=\"Chasing Cravings\" />
+		<link rel=\"stylesheet\" href=\"css/navbar.css\">";
+?>
+		
