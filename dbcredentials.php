94f3af85c83b6e05f46ea26736ef3f11ddf33143
colsson0 <colsson0@gmail.com>
2017-04-07 19:58:42 -0400

    Create dbcredentials.php

---
+++ dbcredentials.php
@@ -0,0 +1,8 @@
+<?php
+	// This file contains the database information and credentials for a database user account
+	// that will prevent access to unneeded fuctions
+	$server = "localhost";
+	$db = "chasingcravings";
+	$user = "user";
+	$pw = "user1234";
+?>
