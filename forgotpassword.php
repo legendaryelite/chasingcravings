bee808d38cc3c3540bafb22f3e9709442474af67
legendaryelite <teetheelite@gmail.com>
2017-04-19 18:37:44 -0400

    Update forgotpassword.php

--- forgotpassword.php
+++ forgotpassword.php
@@ -1,1 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <title>Forgot Passaword</title>
+</head>
+<body>
+  <h4> Forgot your password? Let us help you out. Please Fill Out The Form Below For Us To Better Assist You.</h4>
+<form action="forgotpassword.php" method="$_POST">
+Enter Email <input type="email" name="Email" placeholder="E-Mail" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$" required>*
+</br>
+</br>
+Enter Username <input type="text" name="Username" placeholder="Username">
+</br>
+</br>
+<button type="Submit">Send Password</button>
+<button type="reset">Reset Form</button>
+
+<p>* Indicates a required field</p>
+
+<center></p> If you have forgotten you username and email, feel free to contact us via message centre.</p></center>
+
+</body>
+</html>
 
